040ad9bb76fce07e09e799f2d3f29c60bd14bedf
David Razmadze <razd45@gmail.com>
2022-05-08 17:28:04 -0400

    Add working code for printing eemail and name

--- pages/form.js
+++ pages/form.js
@@ -1,6 +1,22 @@
 import React from "react";
+import { collection, getDocs, doc, getDoc } from "firebase/firestore";
+
+import { db } from "./_app";
 
 function form() {
+  const readData = async () => {
+    const walletAddress = "0x0B84BC8aA0B38F46EdD392B4F0270a870e30AFeF";
+    const docRef = doc(db, "wallets", walletAddress);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  };
+
   return (
     <div>
       <h1>form</h1>
@@ -8,6 +24,8 @@ function form() {
       <p>Wallet Address: </p>
       <p>Name: </p>
       <p>Email Address: </p>
+
+      <button onClick={readData}>Read data</button>
     </div>
   );
 }
