eae2c61d95004567fa714b0fc5eb500bc0730b7b
David Razmadze <razd45@gmail.com>
2022-05-08 16:42:55 -0400

    Add sample-test

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,33 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
+  for (const account of accounts) {
+    console.log(account.address);
+  }
 });
+
+// Add environment variable to your shell. See section in `README.md` file
+
+// Go to https://hardhat.org/config/ to learn more
+module.exports = {
+  solidity: "0.8.9",
+  paths: {
+    artifacts: "./src/artifacts",
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/40e0064815cb4b7f9705bc8c4f1a5b87",
+      accounts: [process.env.METAMASK_PRIVATE_KEY],
+    },
+  },
+  // https://coinsbench.com/verify-smart-contract-on-polygonscan-using-hardhat-9b8331dbd888
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
