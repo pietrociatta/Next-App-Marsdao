270a6a5c7c9b55ff7ffd1adb37a785ca1cba5aa9
pietrociatta <59965830+pietrociatta@users.noreply.github.com>
2022-05-09 11:45:31 +0200

    read and write operation tested and working with the front-end input by users

--- pages/form.js
+++ pages/form.js
@@ -4,18 +4,34 @@ import { doc, getDoc, setDoc } from "firebase/firestore";
 import { db } from "./_app";
 import { useEffect, useState } from "react";
 import { useMoralis } from "react-moralis";
+import { useForm } from "react-hook-form";
 
 function form() {
   const { authenticate, isAuthenticated, user, logout } = useMoralis();
   const [address, setAddress] = useState();
 
+  const [currentAddress, setcurrentAddress] = useState("");
+  const [Name, setName] = useState("");
+  const [Email, setEmail] = useState("");
+  //FORM VARIABLE
+  const onSubmit = (data) => console.log(data);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
   useEffect(() => {
     if (isAuthenticated) {
       setAddress(user.attributes.ethAddress);
+      setcurrentAddress(address);
     } else {
       setAddress("");
     }
   }, [isAuthenticated]);
+
+  //READ OPERATION
 
   const readData = async () => {
     setAddress(user.attributes.ethAddress);
@@ -26,17 +42,27 @@ function form() {
 
     if (docSnap.exists()) {
       console.log("Document data:", docSnap.data());
+      const passAddress = docRef._key.path.segments[1];
+      const passName = docSnap.data().name;
+      const passEmail = docSnap.data().emailAddress;
+      console.log(docRef);
+
+      setcurrentAddress(passAddress);
+      setName(passName);
+      setEmail(passEmail);
     } else {
       // doc.data() will be undefined in this case
       console.log("No such document!");
     }
   };
 
+  //WRTIE OPERATION
+
   const writeData = async () => {
     if (isAuthenticated) {
       await setDoc(doc(db, "wallets", address), {
-        name: "EXAMPLE",
-        emailAddress: "EMAIL",
+        name: Name,
+        emailAddress: Email,
       });
     } else {
       alert("Can not write. Not authenticated");
@@ -47,13 +73,40 @@ function form() {
     <div>
       <h1>form</h1>
 
-      <p>Wallet Address: </p>
-      <p>Name: </p>
-      <p>Email Address: </p>
+      <form className="max-w-md mx-auto ">
+        <input
+          className="w-full"
+          type="text"
+          placeholder="Name"
+          name="email"
+          value={Name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
-      <div>
-        <button onClick={readData}>Read data (Dev purposes)</button>
-        <button onClick={writeData}>Update</button>
+        <input
+          className="w-full"
+          type="text"
+          placeholder="Email"
+          name="email"
+          value={Email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className="w-full"
+          type="text"
+          placeholder="Wallet"
+          name="wallet"
+          value={currentAddress}
+        />
+      </form>
+
+      <div className="flex gap-3 justify-center pt-3">
+        <button className="btn" onClick={readData}>
+          Read data (Dev purposes)
+        </button>
+        <button className="btn btn-primary" onClick={writeData}>
+          Update
+        </button>
       </div>
     </div>
   );
